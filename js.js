da490617e4af3f5f4fe14ef63ab48e8d3572b5cb
Mohamad Nassar <mnassar57@gmail.com>
2022-09-16 13:17:13 +0300

    adding images stylign and html blocks

---
+++ js.js
@@ -0,0 +1,27 @@
+const cardArray = [
+    {
+        name: 'csharp',
+        img: 'images/csharp.png'
+    },
+    {
+        name: 'javascript',
+        img: 'images/js.png'
+    },
+    {
+        name: 'java',
+        img: 'images/java.png'
+    },
+    {
+        name: 'csharp',
+        img: 'images/csharp.png'
+    },
+    {
+        name: 'javascript',
+        img: 'images/js.png'
+    },
+    {
+        name: 'java',
+        img: 'images/java.png'
+    }
+]
+
