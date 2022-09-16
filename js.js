880a79c0ac1234b7ad795ffeac4b37fce3b3d593
Mohamad Nassar <mnassar57@gmail.com>
2022-09-16 13:20:14 +0300

    initializing images through loop

--- js.js
+++ js.js
@@ -29,5 +29,11 @@ let menu = document.getElementById('grid-container');
 let children = menu.children;
 console.log(children.length);
 
+for(let i = 0; i < children.length; i++){
+    let image = document.createElement('img');
+    image.src = cardArray[i].img;
+    image.classList.add("width");
+    children[i].append(image);
+}
 
 
